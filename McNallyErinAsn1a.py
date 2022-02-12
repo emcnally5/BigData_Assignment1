76d2e69fca4f973df03c77a85e3c9221e17d9106
emcnally5 <99140688+emcnally5@users.noreply.github.com>
2022-02-12 10:58:00 -0500

    Add files via upload

---
+++ McNallyErinAsn1a.py
@@ -0,0 +1,7 @@
+# Erin McNally - Assignment 1A
+distance = float(input("Distance (mi)"))
+speed = float(input("Speed (mph)"))
+
+total = int(round(distance/speed, 1))
+
+print("Travel time (hrs)", total)
